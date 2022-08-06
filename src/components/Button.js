5f962e543fe0318277032cdb4db378b93136112b
ysmnclsknnl <101097614+ysmnclsknnl@users.noreply.github.com>
2022-08-06 20:50:31 +0200

    e argument is passed to anonymus function

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Button = ({ func, text, num }) => {
-  return <button onClick={() => func(num)}>{text}</button>;
+  return <button onClick={(e) => func(num)}>{text}</button>;
 };
 
 export default Button;
